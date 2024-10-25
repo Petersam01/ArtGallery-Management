4b663bb4a6c037b7dfa598e82047bd550dd745ba
Petersam01 <wwwpetersam2@gmail.com>
2024-10-25 20:55:24 +0200

    fixing privacy errors

--- ArtGallary/Controllers/Settings.cs
+++ ArtGallary/Controllers/Settings.cs
@@ -9,15 +9,6 @@ namespace ArtGallary.Controllers
     {
         // for using phpmyadmin zampp
        // public const string CONNECTION_STRING = "Database=artgallery;Data Source=127.0.0.1;" +
-       // "User Id=root";
-        //for smaterasp.net
-        //public const string CONNECTION_STRING = "Server=MYSQL8002.site4now.net;Database=db_aad772_gallery;Data Source=208.98.35.132;Uid=aad772_gallery;Pwd=P3t3rs4m";
-        //  public const string CONNECTION_STRING = "Database=if0_37357643_artgallery;" +
-        //   "Data Source=sql205.infinityfree.com;" +
-        // "User Id=if0_37357643;" +
-        //  "Password=P3t3rs4m2psp;";
-
-       public const string CONNECTION_STRING = "Server=MYSQL8002.site4now.net;Database=db_aad772_mkansi0;Uid=aad772_mkansi0;Pwd=P3t3rs4m";
-
+    
     }
 }
